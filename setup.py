634ec8ca7f231b49e335e2e8553d7537b45caae0
Suryasen Singh <suryasen.singh001@gmail.com>
2025-08-31 01:21:22 +0530

    fixes

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
-import os
 from setuptools import setup, find_packages
-
-# Get version from Jenkins (BUILD_VERSION), fallback to default
-VERSION = os.getenv("BUILD_VERSION", "0.1.0")
 
 setup(
     name="myapp",
-    version=VERSION,
-    packages=find_packages(),
+    version="1.0.1",
+    packages=find_packages(),  # <- this includes your myapp/ directory
     install_requires=[
-        "requests",  # example dependency
+        "requests",
     ],
     entry_points={
         "console_scripts": [
-            "myapp = myapp.__main__:main",  # lets you run "myapp"
+            "myapp=myapp.main:main",  # if you have a main.py
         ]
     },
-    python_requires=">=3.6",
 )
